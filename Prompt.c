c326745c8ce15dc3bfd61d3d429721f5370a31e9
R-Mascarenhas <rafaelmascarenhascosta@gmail.com>
2020-07-25 16:14:14 -0300

    add extra function to main

--- Prompt.c
+++ Prompt.c
@@ -1,8 +1,8 @@
 /*
  ============================================================================
  Name        : Prompt.c
- Author      : 
- Version     :
+ Author      : Rafael Mascarenhas Costa
+ Version     : 0.1
  Copyright   : 
  Description : Hello World in C, Ansi-style
  ============================================================================
@@ -11,13 +11,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "linked.h"
+
 
 void print_entry(char *entry) {
-	printf("You entered: %s\n", entry);
+	//printf("You entered: %s\n", entry);
 }
 
 int main(int argc, char *argv[]) {
 	char input[201];
+	list_t* head = NULL;
 
 	while(1) {
 		printf("prompt> ");
@@ -29,6 +32,38 @@ int main(int argc, char *argv[]) {
 		if (strncmp(input, "exit\n", 5) == 0) {
 			printf("Leaving. Good bye.\n");
 			break;
+		}
+		
+		if(strncmp(input,"put", 3) == 0){
+			put(&head, input[4] - '0');
+			list(head);
+		}
+
+		if(strncmp(input,"get", 3) == 0){
+			int index = input[4] - '0' - 1;
+			printf("%d\n",get(head, index));
+		}
+
+		if(strncmp(input,"first", 5) == 0){
+			printf("%d\n",first(head));
+		}
+
+		if(strncmp(input,"last", 4) == 0){
+			printf("%d\n",last(head));
+		}
+
+		if(strncmp(input,"list", 4) == 0){
+			list(head);
+		}
+
+		if(strncmp(input,"clear", 5) == 0){
+			clear(head);
+		}
+
+		if(strncmp(input,"remove", 6) == 0){
+			int index = input[7] - '0' - 1;
+			removeNum(&head, index);
+			list(head);
 		}
 
 		print_entry(input);
