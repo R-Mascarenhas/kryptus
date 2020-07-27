dc3da8233e4d4556bfa525865db15d13970bd25a
R-Mascarenhas <rafaelmascarenhascosta@gmail.com>
2020-07-26 21:33:44 -0300

    make main cleaner

--- Prompt.c
+++ Prompt.c
@@ -14,13 +14,10 @@
 #include "linked.h"
 
 
-void print_entry(char *entry) {
-	//printf("You entered: %s\n", entry);
-}
-
-int main(int argc, char *argv[]) {
+int main() {
+	
 	char input[201];
-	list_t* head = NULL;
+	list_t* start = NULL;
 
 	while(1) {
 		printf("prompt> ");
@@ -34,39 +31,8 @@ int main(int argc, char *argv[]) {
 			break;
 		}
 		
-		if(strncmp(input,"put", 3) == 0){
-			put(&head, input[4] - '0');
-			list(head);
-		}
+		analyze(input, &start);
 
-		if(strncmp(input,"get", 3) == 0){
-			int index = input[4] - '0' - 1;
-			printf("%d\n",get(head, index));
-		}
-
-		if(strncmp(input,"first", 5) == 0){
-			printf("%d\n",first(head));
-		}
-
-		if(strncmp(input,"last", 4) == 0){
-			printf("%d\n",last(head));
-		}
-
-		if(strncmp(input,"list", 4) == 0){
-			list(head);
-		}
-
-		if(strncmp(input,"clear", 5) == 0){
-			clear(head);
-		}
-
-		if(strncmp(input,"remove", 6) == 0){
-			int index = input[7] - '0' - 1;
-			removeNum(&head, index);
-			list(head);
-		}
-
-		print_entry(input);
 	}
 
 	return EXIT_SUCCESS;
