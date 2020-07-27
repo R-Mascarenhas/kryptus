cb5a3c5aa8268af87a3230f3f40d4c7f73b0f115
R-Mascarenhas <rafaelmascarenhascosta@gmail.com>
2020-07-26 21:32:55 -0300

    add extra function to modularize more

--- include/linked.h
+++ include/linked.h
@@ -4,19 +4,23 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef struct list{
     int num;
     struct list *next;
 } list_t;
 
-void put(list_t** hRef, int data);
-int get(list_t* head, int index);
+list_t *createNode(int num);
+void put(list_t** start, int data);
+int get(list_t* start, int index);
 void list(list_t* node);
-void removeNum(list_t** hRef, int index);
-int first(list_t* head);
-int last(list_t* head);
-void clear(list_t* head);
+void removeNum(list_t** start, int index);
+int first(list_t* start);
+int last(list_t* start);
+void clear(list_t **start);
+void analyze(char *input, list_t **start);
+void command(char *function, int num, list_t **start);
 
 
 #endif // ! __LINKED_H__
--- linked.c
+++ linked.c
@@ -1,61 +1,120 @@
 
 #include "linked.h"
 
-
-void put(list_t** hRef, int data){
-	list_t* newNode = (list_t*) malloc(sizeof(list_t));
-	
-	list_t *last = *hRef;
-
-	newNode->num = data;
-    newNode->next = NULL;
+void analyze(char *input, list_t **start){
     
-	if (*hRef == NULL)  
-    {  
-        *hRef = newNode; 
-        return;
+    char function[10];
+    memset(function, '\0', sizeof(function));
+
+    int num = 0;
+    int i = 0;
+
+    for(i = 0; input[i] != '\0' && input[i] != ' '; i++){
+        function[i] = input[i];
+	}
+
+    if(input[i] == ' '){
+        for(++i; input[i] != '\n'; i++){
+            num = num*10 + (input[i] - '0');
+        }
     }
+
+	command(function, num, start);
     
-    while (last->next != NULL){  
-        last = last->next;
-    }
-    last->next = newNode;  
 }
 
-int get(list_t* head, int index){
-	list_t* current = head; 
+void command(char * function, int num, list_t **start){
+	
+	if(strncmp(function,"put", 3) == 0){
+			put(start, num);
+			list(*start);
+
+		} else if(strncmp(function,"get", 3) == 0){
+			int index = num - 1;
+			printf("%d\n",get(*start, index));
+
+		} else if(strncmp(function,"first", 5) == 0){
+			printf("%d\n",first(*start));
+
+		} else if(strncmp(function,"last", 4) == 0){
+			printf("%d\n",last(*start));
+
+		} else if(strncmp(function,"list", 4) == 0){
+			list(*start);
+
+		} else if(strncmp(function,"clear", 5) == 0){
+			clear(start);
+
+		} else if(strncmp(function,"remove", 6) == 0){
+			int index = num - 1;
+			removeNum(start, index);
+			list(*start);
+		}
+}
+
+
+void put(list_t **start, int data){
+	
+    list_t *newNode = createNode(data);
+	list_t *last = *start;
+
+    if(*start == NULL)
+        *start = newNode;
+        
+    else{ 
+        
+        while (last->next != NULL)
+            last = last->next;
+    
+    last->next = newNode;  
+
+    }
+}
+
+int get(list_t* start, int index){
+	list_t* current = start; 
       
     int aux = 0; 
     while (current != NULL) 
     { 
-        if (aux == index) {
+        if (aux == index) 
             return(current->num); 
-        }
+    
         aux++; 
         current = current->next; 
     }
+
+    return -1;
 }
 
-void list(list_t *node) 
+void list(list_t *start) 
 { 
-  while (node != NULL) 
-  { 
-     printf("%d ", node->num); 
-     node = node->next; 
-  } 
-  printf("\n");
+    list_t *node = start;
+
+    while (node != NULL) 
+    { 
+        printf("%d ", node->num); 
+        node = node->next; 
+    }
+
+    if(start == NULL)
+        printf("The list is empty.");
+    printf("\n");
+
+    free(node);
 } 
 
-void removeNum(list_t **hRef, int index) 
+void removeNum(list_t **start, int index) 
 { 
-   if (*hRef == NULL) 
+   if (*start == NULL) 
       return; 
   
-   list_t* previous = *hRef; 
+   list_t* previous = *start; 
   
     if (index == 0) 
     { 
-        *hRef = previous->next;   
+        *start = previous->next;  
+        printf("%d\n", (previous->num)); 
         free(previous);               
         return; 
     } 
@@ -70,18 +129,18 @@ void removeNum(list_t **hRef, int index)
   
     list_t *next = previous->next->next; 
     
-    printf("%d\n", *(previous->next));
+    printf("%d\n", (previous->next->num));
     free(previous->next);
   
     previous->next = next;  
 } 
 
-int first(list_t* head){
-	return head->num;
+int first(list_t* start){
+	return start->num;
 }
 
-int last(list_t* head){
-	list_t* current = head; 
+int last(list_t* start){
+	list_t* current = start; 
     if (current == NULL){
         return -1;
     }
@@ -91,12 +150,21 @@ int last(list_t* head){
     return current->num;
 }
 
-void clear(list_t *head) {
-    list_t *ptr = head;
+void clear(list_t **start) {
+    list_t *ptr = *start;
 
     while (ptr != NULL) {
-        head = ptr;
+        *start = ptr;
         ptr = ptr->next;
-        free(head);
+        free(*start);
     }
+
+    *start = NULL;
+}
+
+list_t *createNode(int num){
+    list_t *newNode = (list_t *)malloc(sizeof(list_t));
+    newNode->num = num;
+    newNode->next = NULL;
+    return newNode;
 }
